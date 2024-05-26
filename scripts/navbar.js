4be4c4e10cb0699ea3ea5d38ed44da69dd8a14e7
Jacques Coetzee <jcoetzee2201@gmail.com>
2024-05-26 22:40:52 +0200

    test3

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -13,8 +13,8 @@ function initialize() {
     const nav = document.querySelector("header > div> nav")
     const ul = document.createElement("ul")
     const link = document.createElement("link")
-    if (document.title == "Home") { link.href = "/styles/styleSheet1.css" }
-    else { link.href = `${root}/styles/styleSheet1.css` }
+    if (document.title == "Home") { link.setAttribute("href","/styles/styleSheet1.css") }
+    else { link.setAttribute("href",`${root}/styles/styleSheet1.css`) }
 
     link.type = "text/css"
     link.rel = "stylesheet"
