f86bac93352c9d841897f7e99b89c5c8f7403de0
Jacques Coetzee <jcoetzee2201@gmail.com>
2024-05-26 22:32:39 +0200

    tsting home

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -13,7 +13,9 @@ function initialize() {
     const nav = document.querySelector("header > div> nav")
     const ul = document.createElement("ul")
     const link = document.createElement("link")
-    link.href = `${root}/styles/styleSheet1.css`
+    if (document.title == "Home") { link.href = "./styles/stylesheet1.css" }
+    else { link.href = `${root}/styles/styleSheet1.css` }
+
     link.type = "text/css"
     link.rel = "stylesheet"
     link.media = "screen,print"
